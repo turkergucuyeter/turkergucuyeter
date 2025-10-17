a35ce9988e3aed8a22dbd11357cab74bdd6550a8
turkergucuyeter <gucuyeterturker@gmail.com>
2025-10-17 23:39:25 +0300

    Delete backend directory

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import env from './config/env.js';
-import app from './server.js';
-
-const port = Number(env.PORT);
-
-app.listen(port, () => {
-  console.log(`Attendance API listening on port ${port}`);
-});
